ea8de7a73ea9bed6e316753caaa62e3ac83196d0
mattdamon108 <woonki.moon@gmail.com>
2019-05-13 14:49:23 +0900

    gw v0.3.0

--- gw.rb
+++ gw.rb
@@ -1,8 +1,8 @@
 class Gw < Formula
   desc "A Wapper a command to watch a filesystem change"
   homepage "https://github.com/mattdamon108/gw"
-  url "https://github.com/mattdamon108/gw/releases/download/v0.2.0/gw-0.2.0.tar.gz"
-  sha256 "4466142caf8da5a8ef38009a0b664010c4e5bc9941e928bf968b77098f22d370"
+  url "https://github.com/mattdamon108/gw/releases/download/v0.3.0/gw-0.3.0.tar.gz"
+  sha256 "c4c9bbb1d1bf3bc135d64bd831fca55c01a5f161ba52a987a2ed7fb97b3b5907"
 
   bottle :unneeded
 
