106bc6caff1fa29aad5b1d9cdf497e1295999ecf
mattdamon108 <woonki.moon@gmail.com>
2019-05-22 00:14:43 +0900

    gqlmerge v0.2.0

--- gqlmerge.rb
+++ gqlmerge.rb
@@ -1,8 +1,8 @@
 class Gqlmerge < Formula
   desc "A merge and stitch tool for GraphQL schema"
   homepage "https://github.com/mattdamon108/gqlmerge"
-  url "https://github.com/mattdamon108/gqlmerge/releases/download/v0.1.4/gqlmerge-0.1.4.tar.gz"
-  sha256 "445e178fd6a5040e67f1b4986203bc21f48643f39d089b2fa52116a3c301da1c"
+  url "https://github.com/mattdamon108/gqlmerge/releases/download/v0.2.0/gqlmerge-0.2.0.tar.gz"
+  sha256 "c03a5dc3ee7b32c93881400da3cb0279fd9f0b13e0072757ebdaeb14fe2e2b9a"
 
   bottle :unneeded
 
