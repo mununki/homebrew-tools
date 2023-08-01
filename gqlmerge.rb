b415f6d7e9af2622080266cc4065b65c420bc978
Woonki Moon <woonki.moon@gmail.com>
2023-08-01 22:35:19 +0900

    gqlmerge v0.2.6

--- gqlmerge.rb
+++ gqlmerge.rb
@@ -1,8 +1,8 @@
 class Gqlmerge < Formula
   desc "A merge and stitch tool for GraphQL schema"
-  homepage "https://github.com/mattdamon108/gqlmerge"
-  url "https://github.com/mattdamon108/gqlmerge/releases/download/v0.2.3/gqlmerge-0.2.3.tar.gz"
-  sha256 "c912ee33890aa3eb93b64de66be6dc8a9ce67d6ec1c4985b3c1b6e1f1392fe5b"
+  homepage "https://github.com/mununki/gqlmerge"
+  url "https://github.com/mununki/gqlmerge/releases/download/v0.2.6/gqlmerge-0.2.6.tar.gz"
+  sha256 "af21dfde586da9549e83d031bbcefb96d530c8b59846a57f77f0da0f1d35c3f4"
 
   bottle :unneeded
 
