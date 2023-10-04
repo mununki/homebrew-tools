9148fe09cb64bc540a6de93f75616af2043e8c83
mununki <woonki.moon@gmail.com>
2023-10-05 02:57:28 +0900

    gqlmerge 0.2.10

--- Formula/gqlmerge.rb
+++ Formula/gqlmerge.rb
@@ -1,8 +1,8 @@
 class Gqlmerge < Formula
   desc "A merge and stitch tool for GraphQL schema"
   homepage "https://github.com/mununki/gqlmerge"
-  url "https://github.com/mununki/gqlmerge/releases/download/v0.2.9/gqlmerge-0.2.9.tar.gz"
-  sha256 "a249fa1f4bc23042495b999871625084cafcb1b129bc05ade9dd7ceac9076d00"
+  url "https://github.com/mununki/gqlmerge/releases/download/v0.2.10/gqlmerge-0.2.10.tar.gz"
+  sha256 "108f912b684bb6f379733344c23730f0509df7d29cefb90894e598993dde1864"
 
   def install
     bin.install "gqlmerge-macos" => "gqlmerge"
